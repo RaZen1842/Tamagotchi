58d3c2052ee148392f67c6086d3845a595580609
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2023-11-17 11:21:57 +0000

    Created tamagotchi

---
+++ Tamagotchi/Tamagotchi.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class Tamagotchi {
+    private var hunger: Int
+    private var happiness: Int
+    private var energy: Int
+    private var health: String
+    private var age: Int
+    private var weight: Double
+}
--- Tamagotchi/main.swift
+++ Tamagotchi/main.swift
@@ -1,11 +1,1 @@
-//
-//  main.swift
-//  Tamagotchi
-//
-//  Created by Veturi, Shreyas (HJRM) on 17/11/2023.
-//
-
 import Foundation
-
-print("Hello, World!")
-
